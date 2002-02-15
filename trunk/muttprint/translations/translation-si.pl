c44cffb2df2def63b25a8f2aae2a7dfa50b67e7c
bwalle <bwalle@c8e997bf-230f-0410-961d-e6f677ee3014>
2002-02-15 15:36:42 +0000

    Adaption to 0.62.

--- trunk/muttprint/translations/translation-si.pl
+++ trunk/muttprint/translations/translation-si.pl
@@ -59,12 +59,39 @@ v datotekah ~/.muttprintrc ter /etc/Muttprintrc.
 -l [jezik], --lang [jezik]
        Jeziki za sporoèila in tiskanje.
 
+-c [jezikovni_nabor], --charset [jezikovni_nabor]
+       Vhodni nabor: latin1, latin2, latin3, latin4, latin5, latin9,
+       auto (preberite manual preden uporabite "auto").
+
+-e [niz], --date [niz]
+       original: izpi¹e datum, kot ga najde v glavi
+       local:    pretvori v lokalno èasovno obmoèje in jezik
+
+-E [niz], --date-format [niz]
+       oblika niza datuma; glej strftime(3) za natanènej¹i opis
+
+-A [niz], --addressformat [niz]
+     Poda format e-naslova v glavi e-pisma, poglejte si navodila 
+     programa ali dokumentacijo za natanènej¹i opis.
+
 -D, --debug | -noD, --nodebug
        Izpi¹e uporabne podatke v datoteko za dnevnik /tmp/muttprint.log.
 
 -d, --duplex | -nod, --noduplex
        Vkljuèi ali izkljuèi duplex (obojestransko) tiskanje.
-	   
+
+-g [¹tevilka], --topmargin [¹tevilka]
+       Zgornji rob v milimetrih
+
+-G [¹tevilka], --bottommargin [¹tevilka]
+       Spodnji rob v milimetrih
+
+-j [¹tevilka], --leftmargin [¹tevilka]
+       Levi rob v milimetrih
+
+-J [¹tevilka], --rightmargin [¹tevilka]
+       Desni rob v milimetrih   
+
 -2 | -1
        Tiska na eno ali dve strani. Enakovredno "papersave mode".
 
@@ -73,7 +100,16 @@ v datotekah ~/.muttprintrc ter /etc/Muttprintrc.
 
 -q, --rem_quote | -noq, --norem_quote
        Odstrani navedene odstavke pri tiskanju.
+
+-z [velikost], --fontsize [velikost]
+       Velikost èrk: 10pt, 11pt, 12pt (samo vrednosti)
+
+-W [¹tevilka], --wrapmargin [¹tevilka]
+       Definira, kako dolge naj bodo vrstice.
 	   
+-r [datoteka], --rcfile [datoteka]
+       Definira dodatno konfiguracijsko datoteko.   
+       
 EOF
 
 $String{"Lizenz"} = "Ta program se distribuira pod pogoji GPL licence, 
